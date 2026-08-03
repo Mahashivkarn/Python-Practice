d4a629ebf8534a8a2fab27c607366d34c9297fe7
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-08-03 16:04:58 +0530

    Errors.

---
+++ 08_Advanced_Python_Concept/06_Errors.py
@@ -0,0 +1,9 @@
+while True:
+    try:
+        a=int(input("Enter a number: "))
+        b=int(input("Enter a number: "))
+                
+        print(f"Sum is {a+b}")
+        
+    except:
+        print("Error Occured")
