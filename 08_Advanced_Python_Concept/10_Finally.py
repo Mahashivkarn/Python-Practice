7e6f5148e792542b741251d752a99aac08d519ad
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-08-04 06:46:25 +0530

    Final.

---
+++ 08_Advanced_Python_Concept/10_Finally.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+
+try:
+    c=a/b
+    print(c)
+except Exception as e:
+    print(e)
+    
+finally: 
+    print("This is always executed")#this alywas executed whether error occurs or not.
