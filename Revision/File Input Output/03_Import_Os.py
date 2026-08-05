82e22685de79190d4391784a0144f6de6503a96d
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-08-05 10:56:53 +0530

    Imorting OS

--- Revision/File Input Output/03_Import_Os.py
+++ Revision/File Input Output/03_Import_Os.py
@@ -3,3 +3,7 @@ import os
 a =os.listdir("dir")
 print(a)
 
+print(os.getcwd())
+print(os.path.exists("harry"))#Tells us that the file exists or not.
+# os.remove("dhruv.txt") for removing txt file
+os.rmdir("dir")#It only delets empty directries.
