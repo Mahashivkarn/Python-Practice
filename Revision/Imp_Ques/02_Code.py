f30e3804c5428f1fe3507ce517dcf7b76b8a637e
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-08-14 12:19:36 +0530

    Agrpase.

--- Revision/Imp_Ques/02_Code.py
+++ Revision/Imp_Ques/02_Code.py
@@ -1,1 +1,22 @@
-print("Hello")
+import argparse
+
+parser= argparse.ArgumentParser(description ="Simple Calculator")
+
+parser.add_argument("num1",type =float,help="First Number")
+parser.add_argument("num2",type =float,help="Second Number")
+parser.add_argument("opertion", choices=["add","subtract","Div","Mul"] ,help="Opertion to perform")
+
+args=parser.parse_args()
+
+print(args) 
+
+if(args.opertion == "add"):
+    print(f"The result is {args.num1 + args.num2}")
+elif(args.opertion == "subtract"):
+    print(f"The result is{args.num1 - args.num2}")
+elif(args.opertion =="Mul"):
+    print(f"The result is {args.num1 * args.num2}")
+elif(args.opertion =="Div"):
+    print(f"The result is {args.num1 / args.num2} ")
+else:
+    print("Some Error Occured")
