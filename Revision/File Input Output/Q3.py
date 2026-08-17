0a12b6e53dee286e1790aa93347939b22823a9bb
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-08-17 12:50:46 +0530

    Reading line and Printing lines.

---
+++ Revision/File Input Output/Q3.py
@@ -0,0 +1,5 @@
+with open("task.txt","w") as f:
+    f.write("line 1\n")
+    f.write("line 2\n")
+    f.write("line 3\n")
+    
---
+++ Revision/File Input Output/Q4.py
@@ -0,0 +1,2 @@
+with open("task.txt","w") as f:
+    f.write("Task Completed")
