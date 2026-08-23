34491ade693ac6375311c04d616c391a8b8f3c5f
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-08-23 16:08:14 +0530

    Downloading template

--- Flask/main.py
+++ Flask/main.py
@@ -1,1 +1,9 @@
-print("Maintaining the streak")
+from flask import Flask ,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+app.run(debug=True)
