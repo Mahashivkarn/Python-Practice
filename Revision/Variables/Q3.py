d279a4a61466f08779b3fb1072a718414603cfdf
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-09-10 23:19:37 +0530

    Concatinate String.

--- Revision/Variables/Q3.py
+++ Revision/Variables/Q3.py
@@ -3,4 +3,4 @@ age=19
 height_in_meter=1.76
 is_student = True
 
-print(f"The name is {name} age is {age} height is {height_in_meter} and is a student {is_student}")
+print("name is" + name,"age is" + str(age))
