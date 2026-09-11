102e2d12e978bd7223945f366cd3689aeebfaeca
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-09-11 16:41:31 +0530

    While loop for infinte password tries

---
+++ Revision/Conditionals_If_Else/Q5.1.py
@@ -0,0 +1,11 @@
+password = "Dhruv282006"
+
+while True:
+    entered_password = input("Enter your password: ")
+
+    if password == entered_password:
+        print("Correct Password")
+        break
+    else:
+        print("Wrong Password. Try again.")
+    
