6cc6ac4ae45abc23e0ff43345bc4a911d8c83444
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-09-13 23:47:28 +0530

    Dictionary

---
+++ Revision/Dictionary/Q1.1.py
@@ -0,0 +1,11 @@
+student = {"name": "John", "age": 20, "grade": "A"}
+
+print(student.get("name"))
+
+student.update({"grade":"A+"})
+
+print(student.get("grade"))
+
+student.update({"city":"Bulandshahr"})
+
+print(student.get("city"))
