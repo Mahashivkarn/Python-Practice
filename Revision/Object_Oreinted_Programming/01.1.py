9136166aa23fbd58159e4af4c2f2058bc89259f5
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-09-14 12:35:13 +0530

    class.

---
+++ Revision/Object_Oreinted_Programming/01.1.py
@@ -0,0 +1,16 @@
+class eirgth:
+    section = "B"
+    
+    def student(self):
+        print(self)
+        return 30
+    
+    
+s=eirgth()
+
+print(s.student())
+print(s.section)
+
+s1=eirgth()
+
+print(s1.student())
