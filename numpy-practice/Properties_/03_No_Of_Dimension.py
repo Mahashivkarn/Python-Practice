16122b7547139213c9c87d0480892f7cd63d573a
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-09-15 22:57:34 +0530

    Number of dimension identifying.

---
+++ numpy-practice/Properties_/03_No_Of_Dimension.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+a=np.array([[1,2,3],
+            [4,5,6],
+            [7,8,9]])
+b=np.array([1,2])
+c=np.array([[2,3],[4,5],[6,7],[7,9]])
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
