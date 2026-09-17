e74d1ab48d7952d6c2594f09f86177c5da6f90f1
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-09-17 23:00:34 +0530

    File creating.

---
+++ Panda-Practice/02_Save.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = {
+    'Name' : ['Dhruv', 'Ram', 'Vir', 'Shyam'],
+    'Age' : ['20','21','23', '34'],
+    'Place' : ['Delhi','Mumbai','Rajasthan','Maharasthra']
+}
+
+df =pd.DataFrame(data)
+print(df)
+
+df.to_csv('output.csv',index = False)
