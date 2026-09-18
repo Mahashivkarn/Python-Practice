3430e989f309dc893438e2377b68c6857496d7c3
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-09-18 20:52:24 +0530

    4

---
+++ Panda-Practice/08_Example.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+data = {
+    "Name" : ['Ram', 'Shyam', 'Ghanshyam', 'Kali', 'Durga', 'Mata', 'Danav'],
+    "Age" : [23, 35, 45, 54, 56, 1000 ,342],
+    "Salary" : [25000, 34000, 45000, 87000, 120000, 500000, 1000000],
+    "Score" : [45, 67, 54, 68, 75, 81, 90]
+    
+}
+
+n =pd.DataFrame(data)
+
+print(n)
+
+print("Names (single name returns column)")
+
+#Selecting Single Coloumn
+
+name = n["Name"]
+
+print(name)
+
+#Selecting Multiple Columns
+
+subset= n[["Name", "Age"]]
+
+print(subset)
