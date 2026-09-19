29579cc0c70b36f9e29765a78665afe1baa53b4c
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-09-19 17:12:58 +0530

    Using 0r conditions.

---
+++ Panda-Practice/10_Using_OR_Conditions.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    "Name" : ['Ram', 'Shyam', 'Ghanshyam', 'Kali', 'Durga', 'Mata', 'Danav'],
+    "Age" : [23, 35, 45, 54, 7, 1000 ,342],
+    "Salary" : [25000, 34000, 45000, 87000, 120000, 500000, 1000000],
+    "Score" : [45, 67, 54, 68, 75, 81, 90]
+    
+}
+
+n =pd.DataFrame(data)
+
+filtered = n[(n["Age"] > 25) | (n["Salary"] > 80000)]
+print(filtered)
