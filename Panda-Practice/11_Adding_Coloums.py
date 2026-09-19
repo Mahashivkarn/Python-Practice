e6603eb9fe9f18c91f1c4f29716fe9a3e849362c
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-09-19 17:13:12 +0530

    Adding_Columns

---
+++ Panda-Practice/11_Adding_Coloums.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+data = {
+    "Name" : ['Ram', 'Shyam', 'Ghanshyam', 'Kali', 'Durga', 'Mata', 'Danav'],
+    "Age" : [23, 35, 45, 54, 7, 1000 ,342],
+    "Salary" : [25000, 34000, 45000, 87000, 120000, 500000, 1000000],
+    "Score" : [45, 67, 54, 68, 75, 81, 90]
+    
+}
+
+n =pd.DataFrame(data)
+# square brackets n["Column_name"] = some_data
+
+print(n)
+
+n['Diwali Bonus'] = n["Salary"] * 0.1
+print(n)
