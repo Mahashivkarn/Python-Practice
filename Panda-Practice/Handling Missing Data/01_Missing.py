c27d8b690aa9d2bf5903945d1899668202f866e4
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-09-21 18:53:58 +0530

    Missing Data.

---
+++ Panda-Practice/Handling Missing Data/01_Missing.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    "Name" : ['Ram', None, 'Ghanshyam', 'Kali', 'Durga', 'Mata', 'Danav'],
+    "Age" : [23, None, 45, 54, 7, 1000 ,342],
+    "Salary" : [25000, None, 45000, 87000, 120000, 500000, 1000000],
+    "Score" : [45, None, 54, 68, 75, 81, 90]
+    
+}
+
+n =pd.DataFrame(data)
+print(n)
+
+print(n.isnull().sum())
