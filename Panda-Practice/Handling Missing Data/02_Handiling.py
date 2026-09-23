377fc38a482ea7963879af64f88aa2709af99b98
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-09-23 20:44:38 +0530

    Missing value.

--- Panda-Practice/Handling Missing Data/02_Handiling.py
+++ Panda-Practice/Handling Missing Data/02_Handiling.py
@@ -1,2 +1,14 @@
-sum = 2 +2
-print(sum)
+import pandas as pd
+
+data = {
+    "Name" : ['Ram', None, 'Ghanshyam', 'Kali', 'Durga', 'Mata', 'Danav'],
+    "Age" : [23, None, 45, 54, 7, 1000 ,342],
+    "Salary" : [25000, None, 45000, 87000, 120000, 500000, 1000000],
+    "Score" : [45, None, 54, 68, 75, 81, 90]
+    
+}
+
+n =pd.DataFrame(data)
+print(n)
+
+print(n.isnull())
