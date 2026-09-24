70eb659c202b545c958dbe023af1acaeb8cb7482
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-09-24 20:54:20 +0530

    Change.

--- Panda-Practice/Handling Missing Data/02_Handiling.py
+++ Panda-Practice/Handling Missing Data/02_Handiling.py
@@ -4,7 +4,7 @@ data = {
     "Name" : ['Ram', None, 'Ghanshyam', 'Kali', 'Durga', 'Mata', 'Danav'],
     "Age" : [23, None, 45, 54, 7, 1000 ,342],
     "Salary" : [25000, None, 45000, 87000, 120000, 500000, 1000000],
-    "Score" : [45, None, 54, 68, 75, 81, 90]
+    "Score" : [45, None, 54, 68, 75, 81,67]
     
 }
 
