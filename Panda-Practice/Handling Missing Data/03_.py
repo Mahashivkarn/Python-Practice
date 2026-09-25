d9e4993b974b10d608030d0a5c6b5d6382621f5b
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-09-25 20:01:30 +0530

    ,

--- Panda-Practice/Handling Missing Data/03_.py
+++ Panda-Practice/Handling Missing Data/03_.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 data = {
     "Name" : ['Ram', None, 'Ghanshyam', 'Kali', 'Durga', 'Mata', 'Danav'],
-    "Age" : [23, None, 45, 54, 7, 1000 ,342],
+    "Age" : [23, None, 45, 54, 7, 19 ,34],
     "Salary" : [25000, None, 45000, 87000, 120000, 500000, 1000000],
     "Score" : [45, None, 54, 68, 75, 81,67]
     
